44469d7db3b442b2309ebfe7598aa5a20b0c625e
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-11 08:30:44 +0700

    feat:array of object manipulation

--- app.js
+++ app.js
@@ -19,34 +19,38 @@ app.use(express.json());
 
 
 // ========== Route ==========
-app.get("/", home);
-app.get("/contact", contact);
+app.get('/', home)
+app.get('/contact', contact)
 
-app.get("/add_project", add_project);
+app.get('/add_project', project)
+app.post('/add_project', add_project)
+app.post('/delete_project/:id', del_project)
 
-app.get("/detail_project/:id", detail_projectbyId);
-app.get("/detail_project", detail_project);
+app.get('/update_project/:id', update_projectView)
+app.post('/update_project', update_project)
 
-app.get("/testimonial", testimonial);
+app.get('/detail_project/:project_id', detail_projectbyId)
+app.get('/detail_project', detail_project)
+
+app.get('/testimonial', testimonial)
 
 
-const data = [
-    {
-        title: "Title 1",
-        image : "https://cdna.artstation.com/p/assets/images/images/078/736/216/4k/eduardo-pena-michis-forest-ep.jpg?1722949590",
-        description: "description 1"
-    },
-    {
-        title: "Title 2",
-        image : "https://cdnb.artstation.com/p/assets/images/images/078/860/057/small/theo-malheuvre-ekko-beautyshot3.jpg?1723277001",
-        description: "description 2"
-    },
-    {
-        title: "Title 3",
-        image : "https://cdna.artstation.com/p/assets/images/images/077/851/190/small/smile-_z-383840271-723901673110426-2912462321000718505-n.jpg?1720532569",
-        description: "description 3"
-    }
-]
+const data = []
+// {
+//     title: "Title 1",
+//     image : "https://cdna.artstation.com/p/assets/images/images/078/736/216/4k/eduardo-pena-michis-forest-ep.jpg?1722949590",
+//     description: "description 1"
+// },
+// {
+//     title: "Title 2",
+//     image : "https://cdnb.artstation.com/p/assets/images/images/078/860/057/small/theo-malheuvre-ekko-beautyshot3.jpg?1723277001",
+//     description: "description 2"
+// },
+// {
+//     title: "Title 3",
+//     image : "https://cdna.artstation.com/p/assets/images/images/077/851/190/small/smile-_z-383840271-723901673110426-2912462321000718505-n.jpg?1720532569",
+//     description: "description 3"
+// }
 function home(req, res) {
     res.render('index', {data})
 }
@@ -55,8 +59,54 @@ function contact(req, res) {
     res.render('contact')
 }
 
-function add_project(req, res) {
+function project(req, res) {
     res.render('add_project')
+}
+function add_project(req, res) {
+    console.log(req.body)
+
+    const {title, startDate, endDate, discription} = req.body
+
+    const image = "https://cdnb.artstation.com/p/assets/images/images/078/860/057/small/theo-malheuvre-ekko-beautyshot3.jpg?1723277001"
+    
+    const data_Project = {title, startDate, endDate, image, discription}
+    data.unshift(data_Project);
+    
+    res.redirect('/')
+}
+
+function del_project(req, res) {
+    const { id } = req.params
+    
+    data.splice(id, 1)
+    // const dataproject = data.find( index => index.id == id );
+    res.redirect('/')
+}
+
+function update_projectView(req, res) {
+    const { id } = req.params
+    
+    const dataFilter = data[parseInt(id)]
+    dataFilter.id = parseInt(id)
+    
+    res.render('update_project', { data: dataFilter })
+}
+function update_project(req, res) {
+    const {id, title, startDate, endDate, discription} = req.body
+    console.log(req.body)
+    
+    
+    const image = "https://cdnb.artstation.com/p/assets/images/images/078/860/057/small/theo-malheuvre-ekko-beautyshot3.jpg?1723277001"
+    
+    data[parseInt(id)] = {
+        title, 
+        startDate, 
+        endDate, 
+        image, 
+        discription
+    }
+    
+    res.redirect('/')
 }
 
 function detail_project(req, res) {
@@ -64,12 +114,14 @@ function detail_project(req, res) {
 }
 
 function detail_projectbyId(req, res) {
-    const { id } = req.params 
+    const { id } = req.params.project_id
 
-    const dataFilter = data[parseInt(id)]
-    dataFilter.id = parseInt(id)
-    console.log("dataFilter", dataFilter)
-    res.render('detail_project', { data: dataFilter })
+    // const dataproject = data[parseInt(id)]
+    // dataproject.id = parseInt(id)
+
+    const dataproject = data.find( project_id => project_id.id == id );
+
+    res.render('detail_project', { data: dataproject })
 }
 function testimonial(req, res) {
     res.render('testimonial')
